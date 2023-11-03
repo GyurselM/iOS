fbeb587909aa6cb0d7321458fc2d4e6a034ee475
Gyursel Mustan <Gyursel Mustan>
2023-11-03 12:13:26 +0000

    Comprobación del nombre y pulsar botón para saltar a la pagina de juego

--- Tarea1/ViewController.swift
+++ Tarea1/ViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var NameTextField: UITextField!
+    @IBAction func Game(_ sender: Any) {
+        //Comprobamos que en el campo este el nombre para poder dar al boton
+        let userName = NameTextField.text
+        if userName?.isEmpty == false {
+            performSegue(withIdentifier: "toGame", sender: nil)
+        }else{
+            //Guardar el user name
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
